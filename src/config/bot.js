3e17eed1b1dd8074142f6ae86ed13855b094788b
steamhubczvesmir636-jpg <steamhubczvesmir636@gmail.com>
2026-06-06 20:10:28 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Pomůžu se vším jsem 24/7 Aktivní📞",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
